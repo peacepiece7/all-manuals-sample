600e4e60ac8fd5ab874587b075909956c120d338
taetae <peacepiece7@naver.com>
2022-03-15 19:31:48 +0900

    Edit : dummy policy form

--- front/components/PolicyForm/PolicyForm.jsx
+++ front/components/PolicyForm/PolicyForm.jsx
@@ -1,3 +1,33 @@
 export default function PolicyForm() {
-  return <div>Policy</div>;
+  return (
+    <div>
+      <div>
+        <h1>subject 1</h1>
+        <ul>
+          <li>content ...</li>
+          <li>content ...</li>
+          <li>content ...</li>
+          <li>content ...</li>
+        </ul>
+      </div>
+      <div>
+        <h1>subject 1</h1>
+        <ul>
+          <li>content ...</li>
+          <li>content ...</li>
+          <li>content ...</li>
+          <li>content ...</li>
+        </ul>
+      </div>
+      <div>
+        <h1>subject 1</h1>
+        <ul>
+          <li>content ...</li>
+          <li>content ...</li>
+          <li>content ...</li>
+          <li>content ...</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
